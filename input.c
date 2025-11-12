fe38069786750fde34a07e812b70cf82b033b9bb
[Shuayb <Shuaybb1@gmail.com>
2025-11-12 17:11:14 +0100

    minor changes

--- input.c
+++ input.c
@@ -9,9 +9,9 @@ Input input_read(bool* running) {
         if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) *running = false;
     }
     const Uint8* ks = SDL_GetKeyboardState(NULL);
-    in.left  = ks[SDL_SCANCODE_A] || ks[SDL_SCANCODE_LEFT];
+    in.left  = ks[SDL_SCANCODE_Q] || ks[SDL_SCANCODE_LEFT];
     in.right = ks[SDL_SCANCODE_D] || ks[SDL_SCANCODE_RIGHT];
-    in.up    = ks[SDL_SCANCODE_W] || ks[SDL_SCANCODE_UP];
+    in.up    = ks[SDL_SCANCODE_Z] || ks[SDL_SCANCODE_UP];
     in.down  = ks[SDL_SCANCODE_S] || ks[SDL_SCANCODE_DOWN];
     return in;
 }
